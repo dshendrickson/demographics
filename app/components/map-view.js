eb181621a5b3fbfd8e8eb4647feac917896d3e9c
Dale S Hendrickson <dale@dshendrickson.com>
2017-01-19 15:29:09 -0500

    Clean up from test d3 graph

--- app/components/map-view.js
+++ app/components/map-view.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
-var data = [4, 8, 15, 16, 23, 42, 56, 72, 99];
-var x = d3.scaleLinear()
-  .domain([0, d3.max(data)])
-  .range([0, 420]);
-
 var width = 960,
     height = 500,
     centered;
@@ -56,17 +51,6 @@ var g = svg.append("g");
 export default Ember.Component.extend({
 
   didRender() {
-
-    d3.select(".chart")
-      .selectAll("div")
-      .data(data)
-      .enter().append("div")
-      .style("width", function (d) {
-         return x(d) + "px";
-      })
-      .text(function (d) {
-        return d;
-      });
 
     d3.json("vendor/us.json", function(error, us) {
       if (error) throw error;
